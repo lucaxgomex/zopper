4ef9d179f6a19a988fef7984a5f468795b941faa
lucaxgomex <lucas.g.oliv.dev@gmail.com>
2021-07-02 09:47:15 -0300

    created a input through the model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+	name = models.CharField(max_length=300)
+	get_url = models.URLField(max_length=300)
+	date = models.DateField(auto_now_add=True)
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,28 @@
+global list_register
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Register
+from .forms import RegisterForm
 
-# Create your views here.
+list_register = Register.objects.all()
+
 def index(request):
+	form = RegisterForm(request.POST or None)
+
+	if (form.is_valid()):
+		form.save()
 	return render(request, 'polls/index.html', {})
 
 def test(request):
 	#return redirect('/sla/')
 	return render(request, 'polls/test.html', {})
+
+def create_register(request):
+	form = RegisterForm(request.POST or None)
+
+	if (form.is_valid()):
+		form.save()
+	#return render(request, 'polls/index.html', {'form': form})
+
+def return_url():
+	pass
